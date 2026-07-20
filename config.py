03b2c7eb0b947c5050b843fa8dde708171643665
Rafinaks <danil2005895@gmail.com>
2026-07-20 14:17:01 +0200

    Add config with labels, tooltips, and button texts for the ARCT v2.

---
+++ config.py
@@ -0,0 +1,41 @@
+#----------------------
+#       LABELS
+#----------------------
+
+APPNAME_LABEL = "Auto Router Configuration Tool"
+NEW_PASSWORD_LABEL = "New password:"
+DEFAULT_PASSWORD_LABEL = "Def password:"
+ROUTER_IP_LABEL = "Router IP:"
+FIRMWARE_LABEL = "Select Firmware:"
+ISP_LABE = "Select ISP"
+APN_LABEL = "Select APN"
+
+#----------------------
+#       BUTTONS
+#----------------------
+
+CONNECT_BUTTON = "C\nO\nN\nN\nE\nC\nT"
+
+
+#----------------------
+#       TOOLTIPS
+#----------------------
+
+NEW_PASSWORD_TOOLTIP = "The new password that will be set on the router.\nThis replaces the default password after setup.\nChoose something secure."
+DEFAULT_PASSWORD_TOOLTIP = "The current password on the router for SSH connection.\nAfter a factory reset this is always admin01 or the password on the back of the router."
+ROUTER_IP_TOOLTIP = "The IP address of the router.\nWill be changed after setting up different ISP.\nDefault IP = 192.168.1.1"
+FIRMWARE_TOOLTIP = "Select the firmware version for your router model and press update button.\nIf router is not up to date, wait until update finishes."
+ISP_TOOLTIP = "Select the ISP profile for this router.\nEach profile has a different gateway IP.\nAfter setting up Router IP will be changed!"
+APN_TOOLTIP = "Access Point Name (APN) is required for mobile data.\nDifferent providers have different APNs!\nYou can type it manually or choose one."
+
+#----------------------
+#       LOGS
+#----------------------
+
+
+
+#----------------------
+#       STATUS
+#----------------------
+
+ROUTER_STATUS = "✲"
